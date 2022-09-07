ab96c029793a0254b903eb52797229f3a81131fb
Ahmet Yusuf Seymen <yusuf_seymen63@hotmail.com>
2022-09-07 15:37:34 +0300

    Project Added

--- app/src/main/java/com/patika/week3/HomeFragment.kt
+++ app/src/main/java/com/patika/week3/HomeFragment.kt
@@ -1,15 +1,24 @@
 package com.patika.week3
 
 import android.content.Context
+import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-
+import com.bumptech.glide.Glide
+import com.patika.week3.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private lateinit var fragmentHomeBinding: FragmentHomeBinding
+    var number = 9995
+    val STATEKEY = "savedNumber"
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -19,23 +28,77 @@ class HomeFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.v("PATIKADEV","onCreate called.")
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        savedInstanceState: Bundle?): View {
+
+        if(savedInstanceState != null) {
+            val stateNumber = savedInstanceState.getInt(STATEKEY, 0)
+            number = stateNumber
+        }
+
         Log.v("PATIKADEV","onCreateView called.")
-        return inflater.inflate(R.layout.fragment_home,
-            container,
-            false
-        )
+
+        fragmentHomeBinding = FragmentHomeBinding.inflate(inflater)
+        return fragmentHomeBinding.root
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         Log.v("PATIKADEV","onViewCreated called.")
+
+        fragmentHomeBinding.apply {
+            Glide.with(requireContext()).load(R.drawable.gold).into(gifImageView)
+            txvCounter.text = number.toString()
+            btnCount.setOnClickListener {
+                ++number
+                txvCounter.text = number.toString()
+            }
+
+            txvCounter.addTextChangedListener(textWatcherEmail)
+        }
+    }
+    private val textWatcherEmail = object : TextWatcher {
+
+        override fun afterTextChanged(s: Editable?) {}
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            fragmentHomeBinding.apply {
+
+                if (txvCounter.text.toString().toInt() == 9997) {
+                    activity?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+                    Toast.makeText(requireContext(), "I did not forget", Toast.LENGTH_SHORT)
+                        .show()
+                }
+                if (txvCounter.text.toString().toInt() == 9999) {
+                    activity?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+                    Toast.makeText(requireContext(), "Don't worry about it i won't forget :)", Toast.LENGTH_SHORT)
+                        .show()
+                }
+                if (txvCounter.text.toString().toInt() == 10000) {
+                    btnCount.visibility = View.INVISIBLE
+                    Toast.makeText(requireContext(), "Counting completed!", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+            }
+          
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.v("PATIKADEV","onSaveInstanceState called.")
+
+        outState.putInt(STATEKEY,number)
     }
 
     override fun onViewStateRestored(savedInstanceState: Bundle?) {
@@ -61,11 +124,6 @@ class HomeFragment : Fragment() {
     override fun onStop() {
         super.onStop()
         Log.v("PATIKADEV","onStop called.")
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        Log.v("PATIKADEV","onSaveInstanceState called.")
     }
 
     override fun onDestroyView() {
